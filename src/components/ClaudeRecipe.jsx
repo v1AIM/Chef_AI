627cb75b194b487d35edb902350ebd9bea178b3c
root <root@DESKTOP-3GOL4VP>
2024-11-30 16:31:33 +0200

    Modify the AI response

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -2,7 +2,8 @@ import Markdown from "react-markdown";
 
 export default function ClaudeRecipe(props) {
   return (
-    <section className="suggested-recipe-container">
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>Chef Claude Recommends:</h2>
       <Markdown>{props.recipe}</Markdown>
     </section>
   );
